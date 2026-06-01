abce5b5ea57a0b5798786947e542347be1d96006
Oleksandra <olexasviridova@gmail.com>
2026-06-01 15:17:48 +0200

    use service role key

--- api/scrape.js
+++ api/scrape.js
@@ -2,7 +2,7 @@ import RSSParser from "rss-parser";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://wazffysnbmaavoshqpax.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhemZmeXNuYm1hYXZvc2hxcGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDUyODAsImV4cCI6MjA5NTg4MTI4MH0.0ySZenKWRJdrfI5KP8giO6OnuBgx-CUuTsos2c-tXjs";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhemZmeXNuYm1hYXZvc2hxcGF4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDMwNTI4MCwiZXhwIjoyMDk1ODgxMjgwfQ.ahLQhyPEH84G5b11esQGagFMbBNJkLASYgjRowobPxk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const parser = new RSSParser();
